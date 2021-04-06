7b3f787535bad4ecc2832b69d90a5b74161238f2
Jess Arbar <jessarbar@Jesss-MacBook-Air.local>
2021-04-06 08:31:50 -0600

    Declared variables and assigned values.

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Jessica";
+let lastName = "Arbar";
+var age = 39;
+let city = "Denver, CO";
+let passtimes = "reading, hiking, and listening to music";
 
 // Exercise 2
 
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
 // Exercise 3
+
+let myStory = `Hi there! My name is ${fullName} and I live in ${city}. I love ${passtimes}.`
+console.log(myStory);
+
